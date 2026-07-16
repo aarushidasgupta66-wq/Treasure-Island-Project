d896edd46bcd637bfb81e84f94c7e6b58d897ed9
aarushi-py <aarushidasgupta66@gmail.com>
2026-07-16 11:28:09 +0100

    Revise game prompts and outcomes for better storytelling
    Enhanced the narrative of the game by providing more detailed prompts and descriptions for player choices, improving user experience.

--- task.py
+++ task.py
@@ -23,22 +23,22 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-direction=(input('which direction would you like to move?(type: left/right)')).lower()
+direction=(input('You have spawned in a dense forest, the path on which you are standing leads to two directions...\nwhich direction would you like to move?(type: left/right)')).lower()
 boat=0
 door=0
 if direction=="left":
-    boat=input("would you like to wait for a boat or swim?(type: wait/swim)")
+    boat=input("You end up walking for aproximately 15 minutes, then you detect a river bank\n in the distance across the river you see a large gate with a halo surrounding it.\n But you are trying to figure out a way to cross the river.\n By your side you see an enscribed wooden plank which says\n 'Boat Arrives Every 10 Minutes and Commutes to the Other Side of The river'\n you start to decide => \nwould you like to wait for a boat or swim?(type: wait/swim)")
     if boat=="wait":
-        door=input('which door, red, blue or yellow?type:(red/blue/yellow)')
+        door=input('After waiting for 10 minutes, the boat was puntual enough to arrive exactly at the time the wooden plank had mentioned\nYou carefully sit on the creaking bench as the boat automatically sets off,\n the halo of the gate maximises every metre of distance covered\n the other side of the river welcomes a short path that leads to the gate and you willingly step upon it\n The gate automatically opens to three different coloured doors.\n.which door would you like to go in? red, blue or yellow?type:(red/blue/yellow)')
         if door=="red":
-            print('burned by fire. game over')
+            print('You are Burned by fire. game over')
         elif door=="blue":
-            print('eaten by beasts. game over')
+            print('You are eaten by beasts. game over')
         elif door=="yellow":
-            print('you win!')
+            print('The door opens up to a dark atmosphere\n As you grope through insides of the mysteriuos area you discorver a luminiscent Gold chest\n as you begin to reach for it\n The top opens for you and you are awarded an enourmous amount of real gold coins, diamonds, emaralds, sapphires and pearls.\n\n Congratulations you have reached the ultimate treasure of this game!\n you win!')
         else:
             print('game over')
     else:
-        print("attacked by trout. game over")
+        print("As you swim, trouts attack you eventually. You die! Game over")
 else:
-    print("fall into a hole. game over")
+    print("you enter a area closed by bushes. Attacked by a grizzly bear. Game over")
